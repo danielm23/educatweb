f6cd745ddce4bb0cfaba5d9d1dd40b4694327d7b
Prebzs <schnapper4@gmail.com>
2016-12-09 16:14:19 +0100

    merge from master

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,15 @@
 $(document).foundation();
+
+if ($(window).height() >= $(document).height()) {
+    if ($(window).width() > 640) { //if not grid-stacked
+        $("#main-container").height($("#body").height());
+    }
+}
+
+$(window).resize(function () {
+    if ($(window).height() >= $(document).height()) {   //known bug: .height() not yet updated when .resize() is called -> when shrinked, height is to big
+        if ($(window).width() > 640) {
+            $("#main-container").height($("#body").height());
+        }
+    }
+});
