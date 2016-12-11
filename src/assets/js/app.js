e91706558878b1084c603f8c2fa959488e215f12
Prebzs <schnapper4@gmail.com>
2016-12-11 15:31:46 +0100

    UPDATE side height
    add some css and jQuery stuff to set height correct
    delete old height-stuff never used
    update order in css according to html order

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,38 @@
 $(document).foundation();
 
-if ($(window).height() >= $(document).height()) {
-    if ($(window).width() > 640) { //if not grid-stacked
-        $("#main-container").height($("#body").height());
-    }
-}
+//if ($(window).height() >= $(document).height()) {
+//    if ($(window).width() > 640) { //if not grid-stacked
+//        $("#main-container").height($("#body").height());
+//    }
+//}
+
+jQuery(document).ready(function() {
+    setTimeout(function() {
+        if ($(window).width() > 640) { //if not grid-stacked
+            var navHeight = $("#nav-container").height();
+            if (navHeight != 0) {
+                $("#main-inner-container").css("padding-top", navHeight);
+            }
+        }
+    }, 100);
+    setTimeout(function () {
+        if ($(window).width() > 640) { //if not grid-stacked
+            var navHeight = $("#nav-container").height();
+            if (navHeight != 0) {
+                $("#main-inner-container").css("padding-top", navHeight);
+            }
+        }
+    }, 2000);
+});
+
 
 $(window).resize(function () {
-    if ($(window).height() >= $(document).height()) {   //known bug: .height() not yet updated when .resize() is called -> when shrinked, height is to big
-        if ($(window).width() > 640) {
-            $("#main-container").height($("#body").height());
-        }
+    //if ($(window).height() >= $(document).height()) {   //known bug: .height() not yet updated when .resize() is called -> when shrinked, height is to big
+    //    if ($(window).width() > 640) {
+    //        $("#main-container").height($("#body").height());
+    //    }
+    //}
+    if ($(window).width() > 640) { //if not grid-stacked
+        $("#main-inner-container").css("padding-top", navHeight);
     }
 });
